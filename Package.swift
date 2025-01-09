81e94e757f4d765f22c844a62bdf526379ae235e
Matheus Aeroso <7095132+maeroso@users.noreply.github.com>
2025-01-09 10:41:06 -0300

    Enhance GitHub Actions workflow with caching and permissions for macOS and Linux builds

--- Package.swift
+++ Package.swift
@@ -6,12 +6,12 @@ import PackageDescription
 let package = Package(
     name: "HoopProxy",
     platforms: [
-        .macOS(.v13) // Targeting macOS 13 Ventura for latest Swift features
+        .macOS(.v13)  // Targeting macOS 13 Ventura for latest Swift features
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
         .package(url: "https://github.com/LebJe/TOMLKit.git", from: "0.5.0"),
-        .package(url: "https://github.com/vapor/jwt-kit.git", from: "5.0.0-beta.rc")
+        .package(url: "https://github.com/vapor/jwt-kit.git", from: "5.0.0-beta.rc"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -21,7 +21,11 @@ let package = Package(
             dependencies: [
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
                 .product(name: "TOMLKit", package: "TOMLKit"),
-                .product(name: "JWTKit", package: "jwt-kit")
+                .product(name: "JWTKit", package: "jwt-kit"),
+            ],
+            swiftSettings: [
+                .unsafeFlags(["-cross-module-optimization"], .when(configuration: .release)),
+                .unsafeFlags(["-Osize"], .when(configuration: .release)),
             ]
         ),
         .testTarget(name: "HoopProxyTests", dependencies: ["HoopProxy"]),
